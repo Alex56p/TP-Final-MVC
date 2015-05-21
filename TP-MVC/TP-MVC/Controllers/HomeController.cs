7d8e1c0004cd0119f161ebc5f8cd64872834a669
Alex56p <a.p_alex@hotmail.com>
2015-05-21 16:01:21 -0400

    Rechercher fini

--- TP-MVC/TP-MVC/Controllers/HomeController.cs
+++ TP-MVC/TP-MVC/Controllers/HomeController.cs
@@ -32,8 +32,28 @@ namespace TP_MVC.Controllers
             return View();
         }
 
-        
+        [HttpPost]
+        public ActionResult Pokemons(string TB_RechercherPokemon, string Submit)
+        {
+            switch(Submit)
+            {
+                case "Rechercher":
+                    Pokemon[] pokemons = this.Donnees.Pokemons.Where(c => c.NOM_POKEMON.StartsWith(TB_RechercherPokemon)).ToArray();
+                    ViewBag.Pokemons = pokemons;
+                    break;
+                case "Tout":
+                    Pokemons();
+                    break;
+                default:
+                    Pokemons();
+                    break;
+            }
 
+
+            return View();
+        }
+
+        
         public ActionResult Pokemons()
         {
             Pokemon[] pokemons = this.Donnees.Pokemons.ToArray();
@@ -48,10 +68,50 @@ namespace TP_MVC.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Items(string TB_RechercherItem, string Submit)
+        {
+            switch (Submit)
+            {
+                case "Rechercher":
+                    Item[] items = this.Donnees.Items.Where(c => c.NOM_ITEM.StartsWith(TB_RechercherItem)).ToArray();
+                    ViewBag.Items = items;
+                    break;
+                case "Tout":
+                    Items();
+                    break;
+                default:
+                    Items();
+                    break;
+            }
+            
+            return View();
+        }
+
         public ActionResult Attaques()
         {
             Attaque[] attaques = this.Donnees.Attaques.ToArray();
             ViewBag.Attaques = attaques;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Attaques(string TB_RechercherAttaque, string Submit)
+        {
+            switch (Submit)
+            {
+                case "Rechercher":
+                    Attaque[] attaques = this.Donnees.Attaques.Where(c => c.NOM_ATTAQUE.StartsWith(TB_RechercherAttaque)).ToArray();
+                    ViewBag.Attaques = attaques;
+                    break;
+                case "Tout":
+                    Attaques();
+                    break;
+                default:
+                    Attaques();
+                    break;
+            }
+            
             return View();
         }
 
